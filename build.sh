a1ecb67180aeff9525145c2dc07723be217a2b0f
2465549609@qq.com <xuyifeng@xuyifengdeMacBook-Air.local>
2025-09-06 17:50:53 +0800

    feat: 第一阶段完成抽奖部署

---
+++ build.sh
@@ -0,0 +1,1 @@
+docker build -t xu1feng/big-market-front-app:1.0 .
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    target: 'server',
+    output: 'standalone',
     env: {
         API_HOST_URL: process.env.API_HOST_URL
     }
